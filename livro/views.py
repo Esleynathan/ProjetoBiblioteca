b298e0c23b2eb7fbd28f47c7c8326262cacdfe7a
Esley <esleynathan@hotmail.com>
2023-09-04 16:49:40 -0300

    3º Aula - Models.APP, Templates Home e Ver_livros

--- livro/views.py
+++ livro/views.py
@@ -33,3 +33,34 @@ def home(request):
                                             'livros_emprestados': livros_emprestados})
     else:
         return redirect('/auth/login/?status=2')
+
+def ver_livros(request, id):
+    if request.session.get('usuario'):
+        livro = Livros.objects.get(id = id)
+        if request.session.get('usuario') == livro.usuario.id:
+            usuario = Usuario.objects.get(id = request.session['usuario'])
+            categoria_livro = Categoria.objects.filter(usuario = request.session.get('usuario'))
+            emprestimos = Emprestimos.objects.filter(livro = livro)
+            form = CadastroLivro()
+            form.fields['usuario'].initial = request.session['usuario']
+            form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
+            
+            form_categoria = CategoriaLivro()
+            usuarios = Usuario.objects.all()
+
+            livros_emprestar = Livros.objects.filter(usuario = usuario).filter(emprestado = False)
+            livros_emprestados = Livros.objects.filter(usuario = usuario).filter(emprestado = True)
+            
+            return render(request, 'ver_livro.html', {'livro': livro,
+                                                    'categoria_livro': categoria_livro,
+                                                    'emprestimos': emprestimos,
+                                                    'usuario_logado': request.session.get('usuario'),
+                                                    'form': form,
+                                                    'id_livro': id,
+                                                    'form_categoria': form_categoria,
+                                                    'usuarios': usuarios,
+                                                    'livros_emprestar': livros_emprestar,
+                                                    'livros_emprestados': livros_emprestados})
+        else:
+            return HttpResponse('Esse livro não é seu')
+    return redirect('/auth/login/?status=2')
