8887c4165f895b02052cfefdee4fdc2fe3cb25aa
Esley <esleynathan@hotmail.com>
2023-11-16 17:15:13 -0300

    Projeto Biblioteca #9 | Devolução dos livros

--- livro/views.py
+++ livro/views.py
@@ -6,6 +6,7 @@ from .models import Emprestimos, Livros, Categoria
 from .forms import CadastroLivro, CategoriaLivro
 from django import forms
 from django.db.models import Q
+# Create your views here.
 
 def home(request):
     if request.session.get('usuario'):  
@@ -16,7 +17,6 @@ def home(request):
         form = CadastroLivro()
         form.fields['usuario'].initial = request.session['usuario']
         form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)     
-
         form_categoria = CategoriaLivro()
         usuarios = Usuario.objects.all()
 
@@ -34,6 +34,7 @@ def home(request):
                                             'livros_emprestados': livros_emprestados})
     else:
         return redirect('/auth/login/?status=2')
+
 
 def ver_livros(request, id):
     if request.session.get('usuario'):
@@ -65,80 +66,64 @@ def ver_livros(request, id):
         else:
             return HttpResponse('Esse livro não é seu')
     return redirect('/auth/login/?status=2')
-
+    
 def cadastrar_livro(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CadastroLivro(request.POST)
         
         if form.is_valid():
             form.save()
             return redirect('/livro/home')
         else:
-            return HttpResponse ('Dados Inválidos')
+            return HttpResponse('DADOS INVÁLIDOS')
 
-def excluir_livro(request, id):    
-    livro = Livros.objects.filter(id = id).delete()        
+def excluir_livro(request, id):
+    livro = Livros.objects.get(id = id).delete()
     return redirect('/livro/home')
-
 
 def cadastrar_categoria(request):
     form = CategoriaLivro(request.POST)
     nome = form.data['nome']
     descricao = form.data['descricao']
     id_usuario = request.POST.get('usuario')
-
     if int(id_usuario) == int(request.session.get('usuario')):
         user = Usuario.objects.get(id = id_usuario)
-        categoria = Categoria.objects.filter(nome = nome)
-        if not categoria:
-            categoria = Categoria(nome = nome,
-                                descricao = descricao,
-                                usuario = user)
-            categoria.save()
-            return redirect('/livro/home?cadastro_categoria=1')
-        else:
-            return HttpResponse('Categoria Já existe')
-    else:     
-        return HttpResponse("ERRO 500")
+        categoria = Categoria(nome = nome, descricao = descricao, usuario = user )
+        categoria.save()
+        return redirect('/livro/home?cadastro_categoria=1')
+    else:
+        return HttpResponse('Pare de ser um usuário malandrinho. Não foi desta vez.')
 
-def cadastrar_emprestimo(request):    
-    if request.method == "POST":
-        nome_emprestado = request.POST.get('nome_emprestado')  
-        nome_emprestado_anonimo = request.POST.get('nome_emprestado_anonimo')  
+    
+def cadastrar_emprestimo(request):
+    if request.method == 'POST':
+        nome_emprestado = request.POST.get('nome_emprestado')
+        nome_emprestado_anonimo = request.POST.get('nome_emprestado_anonimo')
         livro_emprestado = request.POST.get('livro_emprestado')
-
+        
         if nome_emprestado_anonimo:
-            emprestimo =   Emprestimos(nome_emprestado_anonimo = nome_emprestado_anonimo,
+            emprestimo = Emprestimos(nome_emprestado_anonimo = nome_emprestado_anonimo,
                                     livro_id = livro_emprestado)
         else:
-            emprestimo =   Emprestimos(nome_emprestado_id = nome_emprestado,
+            emprestimo = Emprestimos(nome_emprestado_id=nome_emprestado,
                                     livro_id = livro_emprestado)
-
         emprestimo.save()
 
         livro = Livros.objects.get(id = livro_emprestado)
         livro.emprestado = True
         livro.save()
 
-        return HttpResponse('Olá')
 
-def devolver_emprestimo(request):    
-    if request.method == "POST":
-        nome_emprestado = request.POST.get('nome_emprestado')  
-        nome_emprestado_anonimo = request.POST.get('nome_emprestado_anonimo')  
-        livro_emprestado = request.POST.get('livro_emprestado')
+        return redirect('/livro/home')
 
-        if nome_emprestado_anonimo:
-            emprestimo =   Emprestimos(nome_emprestado_anonimo = nome_emprestado_anonimo,
-                                    livro_id = livro_emprestado)
-        else:
-            emprestimo =   Emprestimos(nome_emprestado_id = nome_emprestado,
-                                    livro_id = livro_emprestado)
+def devolver_livro(request):
+    id = request.POST.get('id_livro_devolver')
+    livro_devolver = Livros.objects.get(id = id)
+    livro_devolver.emprestado = False
+    livro_devolver.save()
+    
+    emprestimo_devolver = Emprestimos.objects.get(Q(livro = livro_devolver) & Q(data_devolucao = None) )
+    emprestimo_devolver.data_devolucao = datetime.now() 
+    emprestimo_devolver.save()
 
-        emprestimo.save()
-
-        livro = Livros.objects.get(id = livro_emprestado)
-        livro.emprestado = True
-        livro.save()
-
-        return HttpResponse('Olá')
+    return redirect('/livro/home')
